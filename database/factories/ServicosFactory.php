ab8120aa03876c52658dee7c0326f82c25254a91
ThierryMatheus <thierrymatheusreal@gmail.com>
2022-06-17 09:49:15 -0300

    GH-12 Corrigindo erros no arquivo da Factory de servicos

--- database/factories/ServicosFactory.php
+++ database/factories/ServicosFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Categoria;
+use App\Models\Prestador;
 
 class ServicosFactory extends Factory
 {
@@ -14,14 +16,11 @@ class ServicosFactory extends Factory
     public function definition()
     {
         return [
-            'prestador_id'=>1,
-            'categria_id'=>1,
-
+            'prestador_id' => Prestador::All()->random()->id,
+            'categoria_id' => Categoria::All()->random()->id,
             'hora' => $this ->faker->randomNumber(1,true),
             'preco' => $this ->faker->randomFloat(1,20,30),
             'descricao' => $this ->faker->text(),
-            
-        
         ];
     }
 }
