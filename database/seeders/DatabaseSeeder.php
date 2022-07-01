6efef3a947d2b636a9ff93caccd60a2a5af8c2bc
Ranieri Valença <ranierivalenca@gmail.com>
2022-07-01 15:24:25 -0300

    organizando factory, seed, controle de prestador e usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Prestador;
 use App\Models\ServicoContrato;
 use App\Models\User;
 use App\Models\Servicos;
@@ -17,46 +18,38 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-        User::factory(10)->create();
+        // User::factory(10)->create();
+        User::factory()
+            ->count(10)
+            ->sequence(
+                fn ($sequence) => ['email' => 'user'.$sequence->index.'@email.com']
+            )
+            ->create();
+
+        // for ($i = 0; $i < 10; $i++) {
+        //     $user = User::factory()->create();
+        //     Prestador::factory()->create([
+        //         'user_id' => $user->id
+        //     ]);
+        // }
+        for ($i = 0; $i < 10; $i++) {
+            Prestador::factory()
+                ->for(User::factory()->state([
+                    'email' => 'prest'.$i.'@email.com'
+                ]))
+                ->create();
+        }
         //Tutor::factory(10)->create();
 
-      
-        
        
        $this->call([
-   
-           AnimalSeeder::class
-   
-       ]);
-       $this->call([
-        
-            PrestadorSeeder::class
-
-        ]);
-        $this->call([
-
-            CategoriaSeeder::class
-
-
-        ]);
-        $this->call([
-
-           ServicosSeeder::class
-
-
-       ]);
-        $this->call([
-
+            AnimalSeeder::class,
+            CategoriaSeeder::class,
+            ServicosSeeder::class,
             ContratoSeeder::class
-
-
         ]);
         
 
-
-
-
-       
 
     }
 }
